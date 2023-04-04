da00c64fe9299d8a6b372ad06b25a1a85115a044
ma27inranma <ma27inranma@outlook.jp>
2023-04-05 07:09:01 +0900

    many bug fixes. array in default is now supported.

--- ESON.js
+++ ESON.js
@@ -28,235 +28,267 @@ module.exports = ESON
 
 
 const parser = {
-    getchildren: function (eson = "") {
-      eson = eson.trimStart();
-      let obj = {};
-      while (true) {
-        if (eson.startsWith("}") || eson.startsWith("]")) {
+  getchildren: function (eson = "") {
+    eson = eson.trimStart();
+    let obj = {};
+    while (true) {
+      if (eson.startsWith("}") || eson.startsWith("]")) {
+        eson = eson.substring(1).trimStart();
+        if (eson.startsWith(",")) {
           eson = eson.substring(1).trimStart();
-          if (eson.startsWith(",")) {
-            eson = eson.substring(1).trimStart();
-          }
-          return [obj, eson];
         }
-        if (eson.startsWith("[")) {
-          //array
-          let array = [];
-          eson = eson.substring(1);
-          while (true) {
-            eson = eson.trimStart();
-            if (eson.startsWith("]")) {
-              //stop
-              eson = eson.substring(1).trimStart();
-              if (eson.startsWith(",")) {
-                eson = eson.substring(1).trimStart();
-              }
-              return [array, eson];
-            }
-            const commapos = eson.indexOf(",");
-            if (commapos == -1 || commapos > eson.indexOf("]")) {
-              const tempEson = eson.substring(0, eson.indexOf("]")) + ",";
-              eson = tempEson + eson.substring(eson.indexOf("]"));
-            }
-            if (eson.startsWith("'")) {
-              eson = eson.substring(1);
-              const value = eson
-                .substring(0, eson.indexOf("'"))
-                .replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, "'");
-              eson = eson.substring(eson.indexOf("'") + 1);
-              array.push(value);
-            } else if (eson.startsWith("{")) {
-              eson = eson.substring(1);
-              const tempEson = this.getchildren(eson);
-              eson = tempEson[1];
-              array.push(tempEson[0]);
-            } else if (eson.startsWith("[")) {
-              eson = eson.substring(1);
-              const tempEson = this.getchildren(eson);
-              eson = tempEson[1];
-              array.push(tempEson[0]);
-            } else {
-              const value = eson
-                .substring(0, eson.indexOf(","))
-                .trimEnd()
-                .replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, "'");
-              eson = eson.substring(eson.indexOf(",") + 1);
-              array.push(value);
-            }
-          }
-        }
-        if (!eson.includes("=")) {
-          throw new Error("Invalid ESON format.");
-        }
-        const currentObjName = eson.substring(0, eson.indexOf("=")).trimEnd();
-        eson = eson.substring(eson.indexOf("=") + 1).trimStart();
-        const commapos = eson.indexOf(",");
-        if (commapos == -1) {
-          const tempEson = eson.substring(0, eson.indexOf("}")) + ",";
-          eson = tempEson + eson.substring(eson.indexOf("}"));
-        }
-        if (eson.indexOf("}") < commapos) {
-          const tempEson = eson.substring(0, eson.indexOf("}")) + ",";
-          eson = tempEson + eson.substring(eson.indexOf("}"));
-        }
-        if (eson.startsWith("'")) {
-          eson = eson.substring(1);
-          const value = eson
-            .substring(0, eson.indexOf("'"))
-            .replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, "'");
-          eson = eson.substring(eson.indexOf("'") + 1).trimStart();
-          if (eson.startsWith(",")) {
-            eson = eson.substring(1).trimStart();
-          }
-          Object.assign(obj, { [currentObjName]: value });
-        } else if (eson.startsWith("{")) {
-          eson = eson.substring(1);
-          const tempEson = this.getchildren(eson);
-          eson = tempEson[1];
-          Object.assign(obj, { [currentObjName]: tempEson[0] });
-        } else if (eson.startsWith("[")) {
-          //array
-          const tempEson = this.getchildren(eson);
-          eson = tempEson[1];
-          Object.assign(obj, { [currentObjName]: tempEson[0] });
-        } else {
-          const value = eson
-            .substring(0, eson.indexOf(","))
-            .trimEnd()
-            .replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, "'");
-          eson = eson.substring(eson.indexOf(",") + 1).trimStart();
-          Object.assign(obj, { [currentObjName]: value });
-        }
+        return [obj, eson];
       }
-    },
-    parse: function (eson = "") {
-      const date = Date.now();
-      let tryCount = 0;
-      let obj = {};
-      //anti '
-      eson = eson.replace(/\\'/g, "^^^^^[QUOTE]^^^^^");
-      while (true) {
-        tryCount++;
-        if (tryCount > 10) {
-          throw "Unknown Error while parsing ESON.";
-        }
-        eson = eson.trimStart();
-        if (eson.startsWith("}")) {
-          eson = eson.substring(1).trimStart();
-          if (eson.startsWith(",")) {
+      if (eson.startsWith("[")) {
+        //array
+        let array = [];
+        eson = eson.substring(1);
+        while (true) {
+          eson = eson.trimStart();
+          if (eson.startsWith("]")) {
+            //stop
             eson = eson.substring(1).trimStart();
+            if (eson.startsWith(",")) {
+              eson = eson.substring(1).trimStart();
+            }
+            return [array, eson];
           }
-        }
-        if (!eson.includes("=")) {
-          break;
-        }
-        if (eson.startsWith("{")) {
-          eson = eson.substring(1).trimStart();
-          const getChild = this.getchildren(eson);
-          Object.assign(obj, getChild[0]);
-          eson = getChild[1];
-        } else {
-          const currentObjName = eson.substring(0, eson.indexOf("="));
-          eson = eson.substring(eson.indexOf("=") + 1).trimStart();
+          //handle endless loop
+          if(!eson.includes(']')){
+            throw new Error('Invalid ESON format. End of array is not found.');
+          }
           const commapos = eson.indexOf(",");
-          if (commapos > eson.indexOf("}") || commapos == -1) {
-            const tempEson = eson.substring(0, eson.indexOf("}")) + ",";
-            eson = tempEson + eson.substring(eson.indexOf("}")).trimStart();
+          if (commapos == -1 || commapos > eson.indexOf("]")) {
+            const tempEson = eson.substring(0, eson.indexOf("]")) + ",";
+            eson = tempEson + eson.substring(eson.indexOf("]"));
           }
-          if (eson.startsWith("{")) {
+          if (eson.startsWith("'")) {
+            eson = eson.substring(1);
+            const value = eson
+              .substring(0, eson.indexOf("'"))
+              .replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, "'");
+            eson = eson.substring(eson.indexOf("'") + 1).trimStart();
+            if(eson[0]==','){
+              eson=eson.substring(1);
+            }
+            array.push(value);
+          } else if (eson.startsWith("{")) {
             eson = eson.substring(1);
             const tempEson = this.getchildren(eson);
             eson = tempEson[1];
-            Object.assign(obj, { [currentObjName]: tempEson[0] });
-            continue;
+            array.push(tempEson[0]);
+          } else if (eson.startsWith("[")) {
+            // eson = eson.substring(1);
+            const tempEson = this.getchildren(eson);
+            eson = tempEson[1];
+            array.push(tempEson[0]);
+          } else {
+            const value = eson
+              .substring(0, eson.indexOf(","))
+              .trimEnd()
+              .replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, "'");
+            eson = eson.substring(eson.indexOf(",") + 1);
+            array.push(value);
           }
-          const value = eson.substring(0, eson.indexOf(",")).trimEnd();
-          eson = eson.substring(eson.indexOf(",") + 1).trimStart();
-          Object.assign(obj, { [currentObjName]: value });
         }
       }
-      return obj;
-    },
-    strnumToNum: function (obj = {}) {
-      Object.entries(obj).forEach((entry) => {
-        if (entry[1] instanceof Array) {
-          obj[entry[0]] = this.strnumToNum(entry[1]);
-        } else if (entry[1] instanceof Object) {
-          obj[entry[0]] = this.strnumToNum(entry[1]);
-        } else if (typeof entry[1] == "string") {
-          const numbered = Number(entry[1]);
-          if (isNaN(numbered)) {
-            return;
-          } else {
-            obj[entry[0]] = numbered;
-          }
-        } else {
-          throw "Invalid entry type. entry:" + typeof entry[1];
+      if (!eson.includes("=")) {
+        throw new Error("Invalid ESON format current process: "+eson);
+      }
+      let currentObjName;
+      if(eson[0]=='\''){
+        eson=eson.substring(1);
+        currentObjName=eson.substring(0,eson.indexOf('\'')).replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, '\'');
+        eson=eson.substring(eson.indexOf('\'')+1).trimStart();
+        if(eson[0]=='='){
+          eson=eson.substring(1).trimStart();
         }
-      });
-      return obj;
-    },
-    stringify: function (obj = {}) {
-      let stringified = JSON.stringify(obj).replace(/'/g, "\\'");
-      let separated = stringified.split('"');
-      let currentPosition = 0;
-      // return stringified.replace(/(\":)/g, '=').replace(/\"/g, '');
-      let count = 0;
-      while (true) {
-        count++;
-        if (separated.length < 2) {
-          break;
+      }else{
+        currentObjName = eson.substring(0, eson.indexOf("=")).trimEnd();
+        eson = eson.substring(eson.indexOf("=") + 1).trimStart();
+      }
+      const commapos = eson.indexOf(",");
+      if (commapos == -1) {
+        const tempEson = eson.substring(0, eson.indexOf("}")) + ",";
+        eson = tempEson + eson.substring(eson.indexOf("}"));
+      }
+      if (eson.indexOf("}") < commapos) {
+        const tempEson = eson.substring(0, eson.indexOf("}")) + ",";
+        eson = tempEson + eson.substring(eson.indexOf("}"));
+      }
+      if (eson.startsWith("'")) {
+        eson = eson.substring(1);
+        const value = eson
+          .substring(0, eson.indexOf("'"))
+          .replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, "'");
+        eson = eson.substring(eson.indexOf("'") + 1).trimStart();
+        if (eson.startsWith(",")) {
+          eson = eson.substring(1).trimStart();
         }
-        let type = 0;
-        const processing = separated.splice(0, 2);
-        if (processing[1].includes(" ") || processing[1].includes(",")) {
-          const colonchecker = stringified
-            .substring(
-              stringified.indexOf(processing[1], currentPosition) + 2 + processing[1].length
-            )
-            .substring(0, 5);
-          if (colonchecker.startsWith(":")) {
-            //this is objectname
-            const searchStr = `"${processing[1]}":`;
-            const index = stringified.indexOf(searchStr, currentPosition);
-  
-            // stringified =
-            //   stringified.substring(0, index + searchStr.length + 1) +
-            //   stringified.substring(index + searchStr.length + 1);
-            // currentPosition += index + searchStr.length + 1;
-            stringified=stringified.replace(`"${processing[1]}":`, `'${processing[1]}^^^^^'=`);
-          } else {
-            const searchStr = `"${processing[1]}":`;
-            const index = stringified.indexOf(searchStr, currentPosition);
-  
-            // stringified =
-            //   stringified.substring(0, index + searchStr.length + 1) +
-            //   stringified.substring(index + searchStr.length + 1);
-            // currentPosition += index + searchStr.length + 1;
-            stringified=stringified.replace(`"${processing[1]}"`, `'${processing[1]}^^^^^'`);
-          }
-          type = 1;
-        } else {
-          const colonchecker = stringified
-            .substring(
-              stringified.indexOf(`"${processing[1]}"`, currentPosition) + 2 + processing[1].length
-            )
-            .substring(0, 5);
-          if (colonchecker.startsWith(":")) {
-            //this is objectname
-            stringified = stringified.replace(
-              `"${processing[1]}":`,
-              `${processing[1]}^^^^^=`
-            );
-          } else {
-            stringified = stringified.replace(
-              `"${processing[1]}"`,
-              processing[1] + "^^^^^"
-            );
-          }
-          type = 2;
+        Object.assign(obj, { [currentObjName]: value });
+      } else if (eson.startsWith("{")) {
+        eson = eson.substring(1);
+        const tempEson = this.getchildren(eson);
+        eson = tempEson[1];
+        Object.assign(obj, { [currentObjName]: tempEson[0] });
+      } else if (eson.startsWith("[")) {
+        //array
+        const tempEson = this.getchildren(eson);
+        eson = tempEson[1];
+        Object.assign(obj, { [currentObjName]: tempEson[0] });
+      } else {
+        const value = eson
+          .substring(0, eson.indexOf(","))
+          .trimEnd()
+          .replace(/\^\^\^\^\^\[QUOTE\]\^\^\^\^\^/g, "'");
+        eson = eson.substring(eson.indexOf(",") + 1).trimStart();
+        Object.assign(obj, { [currentObjName]: value });
+      }
+    }
+  },
+  parse: function (eson = "") {
+    const date = Date.now();
+    let tryCount = 0;
+    let obj = {};
+    //anti '
+    eson = eson.replace(/\\'/g, "^^^^^[QUOTE]^^^^^");
+    while (true) {
+      tryCount++;
+      if (tryCount > 10) {
+        throw "Unknown Error while parsing ESON.";
+      }
+      eson = eson.trimStart();
+      if (eson.startsWith("}")) {
+        eson = eson.substring(1).trimStart();
+        if (eson.startsWith(",")) {
+          eson = eson.substring(1).trimStart();
         }
       }
-      return stringified.replace(/\^\^\^\^\^/g, "");
-    },
-  };
+      if (!eson.includes("=") && !eson.includes(',')) {
+        break;
+      }
+      if (eson.startsWith("{")) {
+        eson = eson.substring(1).trimStart();
+        const getChild = this.getchildren(eson);
+        Object.assign(obj, getChild[0]);
+        eson = getChild[1];
+      } else {
+        //is input array?
+        const equalpos = eson.indexOf("=");
+        if (equalpos == -1) {
+          //input is array.
+          if(eson[0]!='[') eson="[" + eson + "]";
+        } else if (equalpos > eson.indexOf(",")) {
+          //input is array.
+          if(eson[0]!='[') eson="[" + eson + "]";
+        }
+        //if object ends with }?
+        if ((eson.trimEnd().endsWith("}")==false) && (eson.trimEnd().endsWith("]")==false)) {
+          eson += "}";
+        }
+        const getChild = this.getchildren(eson);
+        return getChild[0];
+        /*const currentObjName = eson.substring(0, eson.indexOf("="));
+        eson = eson.substring(eson.indexOf("=") + 1).trimStart();
+        const commapos = eson.indexOf(",");
+        if (commapos > eson.indexOf("}") || commapos == -1) {
+          const tempEson = eson.substring(0, eson.indexOf("}")) + ",";
+          eson = tempEson + eson.substring(eson.indexOf("}")).trimStart();
+        }
+        if (eson.startsWith("{")) {
+          eson = eson.substring(1);
+          const tempEson = this.getchildren(eson);
+          eson = tempEson[1];
+          Object.assign(obj, { [currentObjName]: tempEson[0] });
+          continue;
+        }
+        const value = eson.substring(0, eson.indexOf(",")).trimEnd();
+        eson = eson.substring(eson.indexOf(",") + 1).trimStart();
+        Object.assign(obj, { [currentObjName]: value });*/
+      }
+    }
+    return obj;
+  },
+  strnumToNum: function (obj = {}) {
+    Object.entries(obj).forEach((entry) => {
+      if (entry[1] instanceof Array) {
+        obj[entry[0]] = this.strnumToNum(entry[1]);
+      } else if (entry[1] instanceof Object) {
+        obj[entry[0]] = this.strnumToNum(entry[1]);
+      } else if (typeof entry[1] == "string") {
+        const numbered = Number(entry[1]);
+        if (isNaN(numbered)) {
+          return;
+        } else {
+          obj[entry[0]] = numbered;
+        }
+      } else {
+        throw "Invalid entry type. entry:" + typeof entry[1];
+      }
+    });
+    return obj;
+  },
+  stringify: function (obj = {}) {
+    let stringified = JSON.stringify(obj).replace(/'/g, "\\'");
+    let separated = stringified.split('"');
+    let currentPosition = 0;
+    // return stringified.replace(/(\":)/g, '=').replace(/\"/g, '');
+    let count = 0;
+    while (true) {
+      count++;
+      if (separated.length < 2) {
+        break;
+      }
+      let type = 0;
+      const processing = separated.splice(0, 2);
+      if (processing[1].includes(" ") || processing[1].includes(",")) {
+        const colonchecker = stringified
+          .substring(
+            stringified.indexOf(processing[1], currentPosition) + 2 + processing[1].length
+          )
+          .substring(0, 5);
+        if (colonchecker.startsWith(":")) {
+          //this is objectname
+          const searchStr = `"${processing[1]}":`;
+          const index = stringified.indexOf(searchStr, currentPosition);
+
+          // stringified =
+          //   stringified.substring(0, index + searchStr.length + 1) +
+          //   stringified.substring(index + searchStr.length + 1);
+          // currentPosition += index + searchStr.length + 1;
+          stringified=stringified.replace(`"${processing[1]}":`, `'${processing[1]}^^^^^'=`);
+        } else {
+          const searchStr = `"${processing[1]}":`;
+          const index = stringified.indexOf(searchStr, currentPosition);
+
+          // stringified =
+          //   stringified.substring(0, index + searchStr.length + 1) +
+          //   stringified.substring(index + searchStr.length + 1);
+          // currentPosition += index + searchStr.length + 1;
+          stringified=stringified.replace(`"${processing[1]}"`, `'${processing[1]}^^^^^'`);
+        }
+        type = 1;
+      } else {
+        const colonchecker = stringified
+          .substring(
+            stringified.indexOf(`"${processing[1]}"`, currentPosition) + 2 + processing[1].length
+          )
+          .substring(0, 5);
+        if (colonchecker.startsWith(":")) {
+          //this is objectname
+          stringified = stringified.replace(
+            `"${processing[1]}":`,
+            `${processing[1]}^^^^^=`
+          );
+        } else {
+          stringified = stringified.replace(
+            `"${processing[1]}"`,
+            processing[1] + "^^^^^"
+          );
+        }
+        type = 2;
+      }
+    }
+    return stringified.replace(/\^\^\^\^\^/g, "");
+  },
+};
