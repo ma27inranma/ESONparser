e4c068e3bf709470b0b611b44004703c9a42d771
ma27inranma <ma27inranma@outlook.jp>
2023-04-21 09:18:20 +0900

    more types

--- ESON.d.ts
+++ ESON.d.ts
@@ -1,7 +1,16 @@
 declare module '@ma27inranma/esonparser'{
     const ESON:{
+        /**
+         * translate ESONtext to object
+         */
         parse(esontext: string): object
+        /**
+         * translate object to esontext
+         */
         stringify(object: object):string
+        /**
+         * convert string that contains only number to Number
+         */
         strNumToNum(object: object):object
     }
 }
